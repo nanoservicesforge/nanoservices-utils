1b62704d48e4b301d182e06c6ddfc2c1a7fab2c5
maxwellflitton <maxwellflitton@gmail.com>
2024-04-17 01:04:22 +0100

    connections now working

--- src/networking/contract.rs
+++ src/networking/contract.rs
@@ -3,26 +3,10 @@ use serde::{Serialize, de::DeserializeOwned};
 
 
 pub trait Contract<D, R>: Sized + Serialize + DeserializeOwned {
-
-    fn handle(
-        message: Self, 
-        handle_fn: fn(Self) -> Result<Self, NanoServiceError>
-    ) -> Result<Self, NanoServiceError> {
-        return handle_fn(message)
-    }
     
     fn data(&self) -> Result<D, NanoServiceError>;
     
     fn result(&self) -> Result<R, NanoServiceError>;
-
-}
-
-
-pub trait ContractHandler {
-
-    fn handle_contract(&self, contract: Vec<u8>) -> Result<Vec<u8>, NanoServiceError>;
-
-    // fn package_contract<T: Contract>(contract: T) -> Result<Self, NanoServiceError>;
 
 }
 
@@ -72,12 +56,12 @@ mod tests {
             result: "World".to_string()
         };
 
-        let result = TestContract::handle(contract, |c| {
-            Ok(c)
-        }).unwrap();
+        // let result = TestContract::handle(contract, |c| {
+        //     Ok(c)
+        // }).unwrap();
 
-        assert_eq!(&result.data().unwrap(), &"Hello".to_string());
-        assert_eq!(&result.result().unwrap(), &"World".to_string());
+        // assert_eq!(&result.data().unwrap(), &"Hello".to_string());
+        // assert_eq!(&result.result().unwrap(), &"World".to_string());
         // assert_eq!(result.unwrap().result(), Ok("World".to_string()));
     }
 }
